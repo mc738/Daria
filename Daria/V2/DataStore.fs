2b7a0d472555b91f1cfe1227924041b1e31f65e6
Max Clifford <me@mclifford.dev>
2024-03-02 13:55:39 +0000

    Added seedFileTypes

--- Daria/V2/DataStore.fs
+++ Daria/V2/DataStore.fs
@@ -1,2 +1,23 @@
-﻿module Daria.V2.DataStore
+﻿namespace Daria.V2
 
+module DataStore =
+
+    open Freql.Sqlite
+    open Daria.V2
+    open Daria.V2.Persistence
+
+    module Initialization =
+
+        let seedFileTypes (ctx: SqliteContext) =
+            FileType.All()
+            |> List.iter (fun ft ->
+                ({ Name = ft.Serialize()
+                   FileExtension = ft.GetExtension()
+                   ContentType = ft.GetContentType() }
+                : Parameters.NewFileType)
+                |> Operations.insertFileType ctx)
+
+
+        ()
+
+    ()
