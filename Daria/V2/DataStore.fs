fcea83c77c405703545d959ca8bf0a08cb577151
Max Clifford <me@mclifford.dev>
2024-03-02 15:26:57 +0000

    Work on DataStore.fs

--- Daria/V2/DataStore.fs
+++ Daria/V2/DataStore.fs
@@ -32,22 +32,39 @@ module DataStore =
                 ({ Name = ct.Serialize() }: Parameters.NewCompressionType)
                 |> Operations.insertCompressionType ctx)
 
-        
         let run (ctx: SqliteContext) =
-            [
-                Records.SettingKeyValue.CreateTableSql()
-                Records.Resource.CreateTableSql()
-                Records.ResourceVersion.CreateTableSql()
-                
-            ]
+            [ Records.SettingKeyValue.CreateTableSql()
+              Records.CompressionType.CreateTableSql()
+              Records.EncryptionType.CreateTableSql()
+              Records.Tag.CreateTableSql()
+              Records.Resource.CreateTableSql()
+              Records.ResourceVersion.CreateTableSql()
+              Records.Image.CreateTableSql()
+              Records.ImagineVersion.CreateTableSql()
+              Records.Template.CreateTableSql()
+              Records.TemplateVersion.CreateTableSql()
+              Records.Artifact.CreateTableSql()
+              Records.ArtifactMetadataItem.CreateTableSql()
+              Records.FileType.CreateTableSql()
+              Records.Series.CreateTableSql()
+              Records.SeriesVersion.CreateTableSql()
+              Records.SeriesVersionMetadataItem.CreateTableSql()
+              Records.SeriesVersionTag.CreateTableSql()
+              Records.Article.CreateTableSql()
+              Records.ArticleVersion.CreateTableSql()
+              Records.ArticleVersionLink.CreateTableSql()
+              Records.ArtifactMetadataItem.CreateTableSql()
+              Records.ArticleVersionTag.CreateTableSql() ]
             |> List.iter (ctx.ExecuteSqlNonQuery >> ignore)
-            
-            
+
+
             seedFileTypes ctx
             seedCompressionTypes ctx
             seedEncryptionTypes ctx
-            
-        
+
+        let runInTransaction (ctx: SqliteContext) =
+            ()
+
         ()
 
     ()
