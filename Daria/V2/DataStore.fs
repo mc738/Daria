eb5a3ad76df3b945371942f74760370f2339060d
Max Clifford <me@mclifford.dev>
2024-03-02 15:20:42 +0000

    Work on DataStore.fs

--- Daria/V2/DataStore.fs
+++ Daria/V2/DataStore.fs
@@ -1,11 +1,14 @@
 ﻿namespace Daria.V2
 
+open Daria.V2
+
 module DataStore =
 
     open Freql.Sqlite
     open Daria.V2
     open Daria.V2.Persistence
 
+    [<RequireQualifiedAccess>]
     module Initialization =
 
         let seedFileTypes (ctx: SqliteContext) =
@@ -17,7 +20,34 @@ module DataStore =
                 : Parameters.NewFileType)
                 |> Operations.insertFileType ctx)
 
+        let seedEncryptionTypes (ctx: SqliteContext) =
+            EncryptionType.All()
+            |> List.iter (fun et ->
+                ({ Name = et.Serialize() }: Parameters.NewEncryptionType)
+                |> Operations.insertEncryptionType ctx)
 
+        let seedCompressionTypes (ctx: SqliteContext) =
+            CompressionType.All()
+            |> List.iter (fun ct ->
+                ({ Name = ct.Serialize() }: Parameters.NewCompressionType)
+                |> Operations.insertCompressionType ctx)
+
+        
+        let run (ctx: SqliteContext) =
+            [
+                Records.SettingKeyValue.CreateTableSql()
+                Records.Resource.CreateTableSql()
+                Records.ResourceVersion.CreateTableSql()
+                
+            ]
+            |> List.iter (ctx.ExecuteSqlNonQuery >> ignore)
+            
+            
+            seedFileTypes ctx
+            seedCompressionTypes ctx
+            seedEncryptionTypes ctx
+            
+        
         ()
 
     ()
