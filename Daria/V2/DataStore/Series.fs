396003dd4b81496a58b3a59a2f7353beb5060a43
Max Clifford <me@mclifford.dev>
2024-03-03 10:56:05 +0000

    Added Series.fs

---
+++ Daria/V2/DataStore/Series.fs
@@ -0,0 +1,21 @@
+﻿namespace Daria.V2.DataStore
+
+[<RequireQualifiedAccess>]
+module Series =
+
+    open Freql.Sqlite
+    open Daria.V2.DataStore.Persistence
+
+
+
+
+    let fetchLatestVersion (ctx: SqliteContext) (seriesId: string) (includeDrafts: bool) =
+        Operations.selectSeriesVersionRecord
+            ctx
+            [ "WHERE series_id = @0"
+              match includeDrafts with
+              | true -> ()
+              | false -> "AND draft = 0" ]
+            [ seriesId ]
+
+        ()
