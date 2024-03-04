4c9b5ae496275605a214c0574fce4c1898e752bd
Max Clifford <me@mclifford.dev>
2024-03-04 23:47:25 +0000

    Added Models.fs

---
+++ Daria/V2/DataStore/Models.fs
@@ -0,0 +1,35 @@
+﻿namespace Daria.V2.DataStore
+
+open System
+
+module Models =
+
+    open Daria.V2.DataStore.Persistence
+
+    type SeriesListingItem =
+        { Id: string
+          Name: string
+          Order: int
+          CreatedOn: DateTime
+          Active: bool
+          Children: SeriesListingItem list
+          Versions: SeriesVersionOverview list }
+
+    and SeriesOverview =
+        { Id: string
+          Name: string
+          Order: int
+          CreatedOn: DateTime
+          Active: bool
+          ParentSeriesId: string option }
+
+    and SeriesVersionOverview =
+        { Id: string
+          Version: string
+          Title: string
+          TitleSlug: string
+          Description: string
+          Hash: string
+          CreatedOn: DateTime
+          Active: bool
+          Draft: bool }
