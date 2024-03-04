d547e10a805b4a7b8f8999ff374d98cff92eca53
Max Clifford <me@mclifford.dev>
2024-03-04 22:57:23 +0000

    Work on Resources.fs

--- Daria/V2/DataStore/Resources.fs
+++ Daria/V2/DataStore/Resources.fs
@@ -1,19 +1,27 @@
 ﻿namespace Daria.V2.DataStore
 
+open Freql.Sqlite
+open Microsoft.FSharp.Core
+
 [<RequireQualifiedAccess>]
 module Resources =
-    
-    module Internal =
-        
-        
-        
-    
-    
-    
-    let fetchLatestVersionDataAsBytes () =
-        
-        ()
-    
-    
-    ()
 
+    open Daria.V2.DataStore.Persistence
+
+    module Internal =
+
+        let fetchLatestVersion (ctx: SqliteContext) (resourceId: string) =
+            Operations.selectResourceVersionRecord
+                ctx
+                [ "WHERE resource_id = @0"; "ORDER BY version DESC"; "LIMIT 1" ]
+                [ resourceId ]
+
+
+
+
+
+    let fetchLatestVersionDataAsBytes (ctx: SqliteContext) (resourceId: string) =
+        Internal.fetchLatestVersion ctx resourceId |> Option.map (fun r -> r.RawBlob.ToString())
+
+
+    ()
