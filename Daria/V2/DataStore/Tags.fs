1d2935c80b3a174a30a460f2b2642f2ce666dab6
Max Clifford <me@mclifford.dev>
2024-03-04 23:46:52 +0000

    Work on Tags.fs

---
+++ Daria/V2/DataStore/Tags.fs
@@ -0,0 +1,19 @@
+﻿namespace Daria.V2.DataStore
+
+open Microsoft.FSharp.Core
+
+module Tags =
+
+    open Freql.Sqlite
+    open Daria.V2.DataStore.Persistence
+
+    let list (ctx: SqliteContext) =
+        Operations.selectTagRecords ctx [] [] |> List.map (fun t -> t.Name)
+
+    let exists (ctx: SqliteContext) (tag: string) =
+        Operations.selectTagRecord ctx [ "WHERE name = @0;" ] [ tag ] |> Option.isSome
+
+    let add (ctx: SqliteContext) (tag: string) =
+        match exists ctx tag with
+        | true -> ()
+        | false -> ({ Name = tag }: Parameters.NewTag) |> Operations.insertTag ctx
