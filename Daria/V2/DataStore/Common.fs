31cd33e01b949a2db7ff7f3f237c723bd79fb2e6
Max Clifford <me@mclifford.dev>
2024-03-05 19:01:08 +0000

    Added toMemoryStream

---
+++ Daria/V2/DataStore/Common.fs
@@ -0,0 +1,60 @@
+﻿namespace Daria.V2.DataStore
+
+open System.IO
+
+module Common =
+
+    let storeVersion = "2"
+
+    [<RequireQualifiedAccess>]
+    type ActiveStatus =
+        | Active
+        | Inactive
+        | All
+
+        member a.ToSqlOption(?prefix: string) =
+            let pf =
+                prefix
+                |> Option.map (fun pf ->
+                    match pf.EndsWith(" ") with
+                    | true -> pf
+                    | false -> $"{pf} ")
+                |> Option.defaultValue ""
+
+            match a with
+            | ActiveStatus.Active -> Some $"{pf}active = TRUE"
+            | ActiveStatus.Inactive -> Some $"{pf}active = FALSE"
+            | ActiveStatus.All -> None
+
+    [<RequireQualifiedAccess>]
+    type DraftStatus =
+        | Draft
+        | NotDraft
+        | All
+
+
+        member d.ToSqlOption(?prefix: string) =
+            let pf =
+                prefix
+                |> Option.map (fun pf ->
+                    match pf.EndsWith(" ") with
+                    | true -> pf
+                    | false -> $"{pf} ")
+                |> Option.defaultValue ""
+            
+            match d with
+            | DraftStatus.Draft -> Some $"{pf}draft = TRUE"
+            | DraftStatus.NotDraft -> Some $"{pf}draft = FALSE"
+            | DraftStatus.All -> None
+
+    let toSql (parts: string list) = parts |> String.concat " "
+    
+    let toMemoryStream (stream: Stream) =
+        match stream with
+        | :? MemoryStream -> stream :?> MemoryStream
+        | _ ->
+            use ms = new MemoryStream()
+            stream.CopyTo(ms)
+            ms
+            
+            
