74e1428e6f516547dbe0c61324025468c83adcca
Max Clifford <me@mclifford.dev>
2024-03-07 20:03:50 +0000

    Work on Images.fs internal

--- Daria/V2/DataStore/Images.fs
+++ Daria/V2/DataStore/Images.fs
@@ -1,18 +1,27 @@
 ﻿namespace Daria.V2.DataStore
 
+open Daria.V2.DataStore.Persistence
 open Freql.Sqlite
 
 module Images =
-    
-    
+
+
     module Internal =
-        
-        ()
+
+
+        let getSpecificVersion (ctx: SqliteContext) (imageId: string) (version: int) =
+            Operations.selectImagineVersionRecord ctx [ "WHERE image_id = @0 AND version = @1" ] [ imageId; version ]
+
+        let getLatestVersion (ctx: SqliteContext) (imageId: string) =
+            Operations.selectImagineVersionRecord
+                ctx
+                [ "WHERE image_id = @0"; "ORDER BY version DESC"; "LIMIT 1" ]
+                [ imageId ]
+
+
 
     let getLatestVersion (ctx: SqliteContext) (imageId: string) =
-        
-        
-        
-        ()
 
-    
+
+
+        ()
