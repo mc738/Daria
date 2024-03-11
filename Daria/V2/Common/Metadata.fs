33cd03c8c29be06cb40032e870c4a1fcfc086e9c
Max Clifford <me@mclifford.dev>
2024-03-11 13:36:16 +0000

    Added Metadata.fs

---
+++ Daria/V2/Common/Metadata.fs
@@ -0,0 +1,63 @@
+﻿namespace Daria.V2.Common
+
+open System
+open System.Globalization
+
+module Metadata =
+
+
+    module Keys =
+
+        let prefix = "daria"
+
+        let seriesId = $"{prefix}:series_id"
+
+        let draft = $"{prefix}:draft"
+
+        let imageId = $"{prefix}:image_id"
+
+        let imageVersion = $"{prefix}:image_version"
+
+        let imageVersionId = $"{prefix}:image_version_id"
+
+        let tags = $"{prefix}:tags"
+
+        let title = $"{prefix}:title"
+
+        let titleSlug = $"{prefix}:title_slug"
+
+        let order = $"{prefix}:order"
+        
+        let createdOn = $"{prefix}:created_on"
+
+    let tryToBool (value: string) =
+        match value.ToLower() with
+        | "true"
+        | "1"
+        | "ok"
+        | "yes" -> Some true
+        | "false"
+        | "0"
+        | "no" -> Some false
+        | _ -> None
+
+    let tryToInt (value: string) =
+        match Int32.TryParse value with
+        | true, v -> Some v
+        | false, _ -> None
+
+    let splitValues (value: string) =
+        value.Split([| ';'; ',' |], StringSplitOptions.RemoveEmptyEntries ||| StringSplitOptions.TrimEntries)
+        |> List.ofArray
+
+    let tryToDateTime (formats: string list) (value: string) =
+        match
+            DateTime.TryParseExact(
+                value,
+                formats |> Array.ofList,
+                CultureInfo.InvariantCulture,
+                DateTimeStyles.AssumeUniversal
+            )
+        with
+        | true, v -> Some v
+        | false, _ -> None
