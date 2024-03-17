79c5e58aa1fbe48b641d0bd70e69d8f3eec0410e
Max Clifford <me@mclifford.dev>
2024-03-17 11:26:09 +0000

    Added Options.fs

---
+++ Daria.App/Common/Options.fs
@@ -0,0 +1,11 @@
+﻿namespace Daria.App.Common
+
+module Options =
+
+    open FsToolbox.AppEnvironment.Args.Mapping
+
+    type AppOptions = | [<CommandValue("import")>] Import of ImportOptions
+
+    and ImportOptions =
+        { [<ArgValue("-p", "--path")>]
+          SettingsPath: string }
