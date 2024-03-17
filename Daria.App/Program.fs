6977171797545b1d67316567f0c59081440a8a9b
Max Clifford <me@mclifford.dev>
2024-03-17 11:26:34 +0000

    Cleaned up Program.fs

--- Daria.App/Program.fs
+++ Daria.App/Program.fs
@@ -1,10 +1,6 @@
-﻿open FsToolbox.AppEnvironment.Args.Mapping
+﻿
 
-type AppOptions = | [<CommandValue("import")>] Import of ImportOptions
 
-and ImportOptions =
-    { [<ArgValue("-p", "--path")>]
-      SettingsPath: string }
 
 // For more information see https://aka.ms/fsharp-console-apps
 printfn "Hello from F#"
