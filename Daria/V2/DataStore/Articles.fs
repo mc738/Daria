e44e9fb09752f81cc5e5d1549ae8c6bf1c87d28d
Max Clifford <me@mclifford.dev>
2024-03-29 12:45:11 +0000

    Updated function name

--- Daria/V2/DataStore/Articles.fs
+++ Daria/V2/DataStore/Articles.fs
@@ -419,7 +419,7 @@ module Articles =
         | false, _ -> AddResult.AlreadyExists id
         | _, false -> AddResult.NoChange id
 
-    let getArticleContent (ctx: SqliteContext) (versionId: string) =
+    let getArticleVersionContent (ctx: SqliteContext) (versionId: string) =
         Operations.selectArticleVersionRecord ctx [ "WHERE id = @0" ] [ versionId ]
         |> Option.map (fun ar -> ar.ArticleBlob.ToBytes() |> Encoding.UTF8.GetString)
 
