35a87bac18d624f6d19cab82ff2cd7d47115d88a
Max Clifford <me@mclifford.dev>
2024-04-07 00:14:41 +0100

    Work on Resources.fs

--- Daria/V2/Operations/Import/Resources.fs
+++ Daria/V2/Operations/Import/Resources.fs
@@ -9,18 +9,26 @@ module Resources =
     type ImageManifestItem =
         { Directory: string
           ImageName: string
-          PreviewName: string option
+          PreviewImageName: string option
           ThanksName: string option
           PreviewUrl: string option }
 
 
         static member TryDeserialize(json: JsonElement) =
-            match
-                Json.tryGetStringProperty "directory" json,
-                Json.tryGetStringProperty "imageName" json
-            with
-            | Some 
-                
+            match Json.tryGetStringProperty "directory" json, Json.tryGetStringProperty "imageName" json with
+            | Some d, Some i ->
+                ({ Directory = d
+                   ImageName = i
+                   PreviewImageName = Json.tryGetStringProperty "previewImageName" json
+                   ThanksName = Json.tryGetStringProperty "thanksName" json
+                   PreviewUrl = Json.tryGetStringProperty "previewUrl" json }
+                : ImageManifestItem)
+                |> Ok
+            | None, _ -> Error "Missing `directory` property"
+            | _, None -> Error "Missing `imageName` property"
 
 
-            ()
+    let importImage (item: ImageManifestItem) =
+        
+        
+        ()
