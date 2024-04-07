ccad89477ff127efafd6831a1a6cf8759dfe528b
Max Clifford <me@mclifford.dev>
2024-04-07 21:27:21 +0100

    Refactored import - added Resources.fs

---
+++ Daria/V2/Operations/Import/Resources.fs
@@ -0,0 +1,26 @@
+﻿namespace Daria.V2.Operations.Import
+
+open System.Text.Json
+open FsToolbox.Core
+
+module Resources =
+
+
+    type ImageManifestItem =
+        { Directory: string
+          ImageName: string
+          PreviewName: string option
+          ThanksName: string option
+          PreviewUrl: string option }
+
+
+        static member TryDeserialize(json: JsonElement) =
+            match
+                Json.tryGetStringProperty "directory" json,
+                Json.tryGetStringProperty "imageName" json
+            with
+            | Some 
+                
+
+
+            ()
