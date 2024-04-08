823d5eb7e2f2e7b13076bf74b36193eeac292ee7
Max Clifford <me@mclifford.dev>
2024-04-08 19:20:57 +0100

    Work on Resources.fs

--- Daria/V2/Operations/Import/Resources.fs
+++ Daria/V2/Operations/Import/Resources.fs
@@ -1,6 +1,13 @@
 ﻿namespace Daria.V2.Operations.Import
 
+open System
+open System.IO
 open System.Text.Json
+open Daria.V2.Common.Domain
+open Daria.V2.DataStore
+open Daria.V2.DataStore.Common
+open Daria.V2.DataStore.Models
+open Freql.Sqlite
 open FsToolbox.Core
 
 module Resources =
@@ -27,8 +34,62 @@ module Resources =
             | None, _ -> Error "Missing `directory` property"
             | _, None -> Error "Missing `imageName` property"
 
+    let tryCreateResourceVersion (path: string) =
+        match File.Exists path with
+        | true ->
+            let extension = Path.GetExtension(path)
+            let name = Path.GetFileNameWithoutExtension(path)
 
-    let importImage (item: ImageManifestItem) =
-        
-        
-        ()
+            { Id = IdType.Generated
+              ResourceId = name
+              ResourceBlob = File.ReadAllBytes path |> Blob.Bytes
+              CreatedOn = Some DateTime.UtcNow
+              FileType = FileType.FromExtension extension
+              EncryptionType = EncryptionType.None
+              CompressionType = CompressionType.None }
+            |> Some
+
+        | false -> None
+
+    let importImage (ctx: SqliteContext) (rootPath: string) (item: ImageManifestItem) =
+
+        let dirPath = Path.Combine(rootPath, item.Directory)
+
+
+        let imagePath = Path.Combine(dirPath, item.ImageName)
+
+        let extension = Path.GetExtension(imagePath)
+        let name = Path.GetFileNameWithoutExtension(imagePath)
+
+
+        match tryCreateResourceVersion imagePath with
+        | Some nrv ->
+            let previewImagePath = 
+            
+            let result =
+                ({ Id = IdType.Generated
+                   ImageId = name
+                   ResourceVersion =
+                     nrv
+                   PreviewResourceVersion =
+                       item.PreviewImageName
+                       
+                       
+                   Url = ""
+                   PreviewUrl = item.PreviewUrl
+                   ThanksHtml =
+                     item.ThanksName
+                     |> Option.bind (fun tn ->
+                         let tnp = Path.Combine(dirPath, tn)
+
+                         match File.Exists tnp with
+                         | true -> File.ReadAllText tnp |> Some
+                         | false -> None) }
+                : NewImageVersion)
+                |> Images.addVersion ctx
+
+            ({ Path = imagePath; Result = result }: ImportResult)
+        | None ->
+            ({ Path = imagePath
+               Result = AddResult.Failure($"File `{imagePath}` not found", None) }
+            : ImportResult)
