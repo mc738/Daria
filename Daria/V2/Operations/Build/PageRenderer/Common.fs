3f45ffc5d804d3e866be1bd0785eb9b7b84db6c9
Max Clifford <me@mclifford.dev>
2024-04-13 22:00:31 +0100

    Refactored Common.fs

--- Daria/V2/Operations/Build/PageRenderer/Common.fs
+++ Daria/V2/Operations/Build/PageRenderer/Common.fs
@@ -1,7 +1,97 @@
 ﻿namespace Daria.V2.Operations.Build.PageRenderer
 
+open Fluff.Core
+
+[<AutoOpen>]
 module Common =
     
+    type ShareLinkType =
+        | Facebook
+        | Twitter
+        | LinkedIn
+        | Email
+
+        static member Deserialize(value: string) =
+            match value.ToLower() with
+            | "facebook" -> Some ShareLinkType.Facebook
+            | "twitter" -> Some ShareLinkType.Twitter
+            | "linkedin" -> Some ShareLinkType.LinkedIn
+            | "email" -> Some ShareLinkType.Email
+            | _ -> None
+
+        static member All() = [ Facebook; Twitter; LinkedIn; Email ]
+
+        member slt.Serialize() =
+            match slt with
+            | Facebook -> "facebook"
+            | Twitter -> "twitter"
+            | LinkedIn -> "linkedin"
+            | Email -> "email"
+
+        member slt.GenerateUrl(articleUrl: string, title: string, description: string) =
+            let linebreak = "%0D%0A"
+
+            match slt with
+            | Facebook -> $"https://www.facebook.com/sharer.php?u={articleUrl}"
+            | Twitter -> $"https://twitter.com/intent/tweet?text={articleUrl}"
+            | LinkedIn -> $"https://linkedin.com/sharing/share-offsite/?url={articleUrl}"
+            | Email -> $"mailto:?subject={title}&body={description}{linebreak}{articleUrl}"
+
+        member slt.GetIcon() =
+            match slt with
+            | Facebook -> "fab fa-facebook"
+            | Twitter -> "fab fa-twitter"
+            | LinkedIn -> "fab fa-linkedin"
+            | Email -> "fas fa-envelope"
+
+        member slt.GetTitle() =
+            match slt with
+            | Facebook -> "Share on Facebook"
+            | Twitter -> "Share on Twitter"
+            | LinkedIn -> "Share on LinkedIn"
+            | Email -> "Share via email"
     
-    ()
+    /// <summary>
+    /// A function to create a local url prefix based on the "depth" of a entity.
+    /// For example, top level entities (i.e. in the main directory) will return ".".
+    /// Entities in a subdirectory (depth 1) will return "..".
+    /// Entities in with a depth 2 will return "../..".
+    /// </summary>
+    /// <param name="depth">The current depth.</param>
+    let createLocalUrlPrefix (depth: int) =
+        match depth >= 1 with
+        | true ->
+            [ for i in 0 .. (depth - 1) do
+                  ".." ]
+            |> String.concat "/"
+        | _ -> "."
+    
+    let createIssueLink (title: string) =
+        title.Replace(" ", "+").Replace("#", "%23")
+        |> fun s -> $"https://github.com/mc738/Articles/issues/new?title=`{s}`+issue"
+
+    let createShareLinksData (articleUrl: string) (title: string) (description: string) =
+        ShareLinkType.All()
+        |> List.map (fun slt ->
+            [ "icon", Mustache.Value.Scalar <| slt.GetIcon()
+              "link_url", Mustache.Value.Scalar <| slt.GenerateUrl(articleUrl, title, description)
+              "link_title", Mustache.Value.Scalar <| slt.GetTitle() ]
+            |> Map.ofList
+            |> Mustache.Value.Object)
+        |> Mustache.Value.Array
+
+    let createLinkData (links: ArticleLink list) =
+        links
+        |> List.map (fun link ->
+            [ "link_title", Mustache.Value.Scalar link.Title
+              "link_description", Mustache.Value.Scalar link.Description
+              "link_url", Mustache.Value.Scalar link.Url ]
+            |> Map.ofList
+            |> Mustache.Value.Object)
+        |> Mustache.Value.Array
+
+    let createTagsData (tags: string list) =
+        tags
+        |> List.map (fun tag -> [ "tag_name", Mustache.Value.Scalar tag ] |> Map.ofList |> Mustache.Value.Object)
+        |> Mustache.Value.Array
 
