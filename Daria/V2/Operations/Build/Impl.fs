2329c7de3b26fa6c058ab44a63d9d81dbccf9c2f
Max Clifford <me@mclifford.dev>
2024-04-14 21:12:58 +0100

    Work on Impl.fs

--- Daria/V2/Operations/Build/Impl.fs
+++ Daria/V2/Operations/Build/Impl.fs
@@ -1,2 +1,37 @@
-﻿module Daria.V2.Operations.Build.Impl
+﻿namespace Daria.V2.Operations.Build
+
+[<AutoOpen>]
+module Impl =
+
+    open System.IO
+    open Fluff.Core
+    open Freql.Sqlite
+    open Daria.V2.DataStore
+    open Daria.V2.Operations.Build.PageRenderer
+    
+    let run storePath =
+        use ctx = SqliteContext.Open storePath
+
+        let rootPath = "C:\\ProjectData\\Articles\\_rendered_v2"
+        let url = "https://blog.psionic.cloud/"
+
+        let pageTemplate =
+            File.ReadAllText "C:\\Users\\44748\\Projects\\Daria\\Resources\\templates\\article.mustache"
+            |> Mustache.parse
+
+        let seriesIndexTemplate =
+            File.ReadAllText "C:\\Users\\44748\\Projects\\Daria\\Resources\\templates\\series_index.mustache"
+            |> Mustache.parse
+
+        let indexTemplate =
+            File.ReadAllText "C:\\Users\\44748\\Projects\\Daria\\Resources\\templates\\index.mustache"
+            |> Mustache.parse
+
+        Series.getTopLevelRenderableSeries ctx
+        |> List.iter (Series.renderSeries ctx pageTemplate seriesIndexTemplate 1 url rootPath)
+
+        Index.renderIndex ctx indexTemplate rootPath
+
+    
+    ()
 
