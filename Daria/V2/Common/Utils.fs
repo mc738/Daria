dcd71c6d98de30f02d926e763064b05b4ad800cf
Max Clifford <me@mclifford.dev>
2024-04-21 20:07:14 +0100

    Work on Utils.fs

---
+++ Daria/V2/Common/Utils.fs
@@ -0,0 +1,34 @@
+﻿namespace Daria.V2.Common
+
+open System
+
+[<AutoOpen>]
+module Utils =
+
+    let resultChoose<'T> (results: Result<'T, _> list) =
+        results
+        |> List.fold
+            (fun acc r ->
+                match r with
+                | Ok v -> v :: acc
+                | Error _ -> acc)
+            []
+        |> List.rev
+
+
+    let resultCollect<'T> (results: Result<'T, string> list) =
+        results
+        |> List.fold
+            (fun (succ, err) r ->
+                match r with
+                | Ok v -> v :: succ, err
+                | Error e -> succ, e :: err)
+            ([], [])
+        |> fun (succ, err) ->
+            match err.IsEmpty with
+            | true -> succ |> List.rev |> Ok
+            | false ->
+                Error(
+                    "There following errors occured:" :: (err |> List.rev)
+                    |> String.concat Environment.NewLine
+                )
