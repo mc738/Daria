6107f07fa57521b50f23024bc5690ff6977a2c48
Max Clifford <me@mclifford.dev>
2024-04-21 20:06:58 +0100

    Work on StoreSettings.fs

---
+++ Daria/V2/Operations/Import/StoreSettings.fs
@@ -0,0 +1,13 @@
+﻿namespace Daria.V2.Operations.Import
+
+open Daria.V2.DataStore
+open Daria.V2.Operations.Common
+open Freql.Sqlite
+
+module StoreSettings =
+    
+    let import (ctx: SqliteContext) (settings: ImportSettings) =
+        settings.StoreSettings
+        |> List.iter (fun s -> Settings.add ctx s.Key s.Value)
+        
+
