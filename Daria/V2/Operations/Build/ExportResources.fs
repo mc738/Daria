4b966b91f431921beb528b46cde7d65cc8552911
Max Clifford <me@mclifford.dev>
2024-04-28 21:04:57 +0100

    Work on ExportResources.fs

--- Daria/V2/Operations/Build/ExportResources.fs
+++ Daria/V2/Operations/Build/ExportResources.fs
@@ -10,9 +10,9 @@ module ExportResources =
     open Daria.V2.DataStore.Common
     open Daria.V2.DataStore
 
-    let exportImages (ctx: SqliteContext) (rootPath: string) =
+    let exportImages (ctx: SqliteContext) (rootPath: string) (directoryName: string) =
 
-        let imgPath = Path.Combine(rootPath, "img")
+        let imgPath = Path.Combine(rootPath, directoryName)
 
         Directory.CreateDirectory(imgPath) |> ignore
 
@@ -40,3 +40,17 @@ module ExportResources =
                         ),
                         rve.Blob
                     ))))
+
+    let exportResourceBucket (ctx: SqliteContext) (bucketName: string) (rootPath: string) (directoryName: string) =
+        let outputPath = Path.Combine(rootPath, directoryName)
+
+        Resources.getBucket ctx bucketName
+        |> List.iter (fun ro ->
+            let latestVersion = ro.Versions |> List.maxBy (fun v -> v.Version)
+
+            let name = Path.GetFileNameWithoutExtension(ro.Name)
+
+            // Could use Resources.fetchLatestVersionAsBytes?
+            Resources.fetchVersionDataAsBytes ctx latestVersion.Id
+            |> Option.iter (fun b ->
+                File.WriteAllBytes(Path.Combine(outputPath, $"{name}{latestVersion.FileType.GetExtension()}"), b)))
